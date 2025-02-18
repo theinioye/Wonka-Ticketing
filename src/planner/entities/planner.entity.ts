4e24ebac5b49f8ae0499e1943aa3c24d825d39ff
Inioluwa Oyetunji <theinioye@gmail.com>
2025-02-18 18:52:05 +0100

    create-modules and entities

--- src/planner/entities/planner.entity.ts
+++ src/planner/entities/planner.entity.ts
@@ -2,13 +2,16 @@ import { BaseModelEntity } from 'src/common/entities/base-model.entity';
 import { Column, Entity } from 'typeorm';
 
 @Entity()
-export class User extends BaseModelEntity {
-  @Column()
-  firstName: string;
-  @Column()
-  lastName: string;
+export class Planner extends BaseModelEntity {
   @Column()
   email: string;
+
+  @Column()
+  name: string;
+
   @Column()
   password: string;
+
+  //   TO-DO:Add Ticket relation
+  // TO-DO:Add event relation
 }
