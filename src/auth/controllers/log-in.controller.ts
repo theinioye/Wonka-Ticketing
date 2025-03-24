c2cbeec26c30d498640b7fb95c6147771607df54
Inioluwa Oyetunji <theinioye@gmail.com>
2025-03-24 16:12:24 +0100

    fix:containerization

--- src/auth/controllers/log-in.controller.ts
+++ src/auth/controllers/log-in.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthPresentationService } from '../presentation-services/auth.presentation-service';
 import { UserSignInDto } from '../dto/user-sign-in.dto';
 import { PlannerSignInDto } from '../dto/planner-sign-in.dto';
+import { Public } from '../decorators/public.decorators';
 
 @Controller('login')
 export class LogInController {
@@ -12,8 +13,14 @@ export class LogInController {
     return this.authService.userLogIn(data);
   }
 
+  @Public()
   @Post('/planner')
   async plannerLogIn(@Body() data: PlannerSignInDto) {
     return this.authService.plannerLogIn(data);
+  }
+
+  @Get()
+  async getHello() {
+    return 'Hello World!';
   }
 }
