d9f5a4be0a4eb2fe2e60a2870a8414b40caa9eac
Inioluwa Oyetunji <theinioye@gmail.com>
2025-03-26 21:05:22 +0100

    chore:payment-logic-test

--- src/Payments/payment.module.ts
+++ src/Payments/payment.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentsController } from './controllers/payment-controller';
 import { Payments } from './entities/payment.entity';
+import { PaymentsPresentationService } from './presentation-services/payment-presentation-service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payments])],
-  providers: [],
+  providers: [PaymentsPresentationService],
   controllers: [PaymentsController],
   exports: [],
 })
